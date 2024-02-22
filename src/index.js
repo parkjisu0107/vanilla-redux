312c5a882da236ff6ccdcd61cb2f1e7452ab7b2e
박지수 <parkjisu0107@gmail.com>
2024-02-22 18:38:39 +0900

    Vanilla ToDo

--- src/index.js
+++ src/index.js
@@ -1,44 +1,30 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-// reducer는 data를 modify하는 함수.
-// modifer와 reducer가 return하는 건 application의 state.
-const countModifier = (count = 0, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
     default:
-      return count;
+      return state;
   }
 };
 
-// store는 data를 저장하는 곳.
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-const onChange = () => {
-  number.innerText = countStore.getState();
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: "ADD_TODO", text: toDo });
 };
 
-// subscribe를 하여 change를 store에서 감지.
-countStore.subscribe(onChange);
-
-// dispatch가 reducer를 불러서 current state와 action 보냄.
-const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
-};
-const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
