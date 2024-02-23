6f59c066f6547152fb66705a162d59838e59de97
박지수 <parkjisu0107@gmail.com>
2024-02-23 16:09:37 +0900

    createReducer

--- src/store.js
+++ src/store.js
@@ -1,19 +1,41 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 import { createStore } from "redux";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+// const reducer = createReducer([], {
+//   [addToDo]: (state, action) => {
+//     state.push({ text: action.payload, id: Date.now() });
+//   },
+//   [deleteToDo]: (state, action) => {
+//     state.filter((toDo) => toDo.id !== action.payload);
+//   },
+// });
+
+// The object notation for `createReducer` is deprecated,
+// and will be removed in RTK 2.0. Please use the 'builder callback' notation
+// instead: https://redux-toolkit.js.org/api/createReducer 해결
+const reducer = createReducer([], (builder) => {
+  builder
+    .addCase(addToDo, (state, action) => {
+      state.push({ id: Date.now(), text: action.payload });
+    })
+    .addCase(deleteToDo, (state, action) => {
       return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+    });
+});
 
 const store = createStore(reducer);
 
